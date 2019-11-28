ce00799c1e34f32ecbcbd2471182f25086d12df1
karlapatrisha <quebec.kp@gmail.com>
2019-11-28 23:02:17 +0800

    initial

---
+++ GoldSplashCasino.FrontEnd/GoldSplashCasino.Portal/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoldSplashCasino.Portal.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Signup()
+        {
+            return View();
+        }
+    }
+}
